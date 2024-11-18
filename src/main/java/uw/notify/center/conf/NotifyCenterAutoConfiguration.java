a5716e68dfd0c998159d9c5a00e933b36d1e7fe8
zhangjin <23231269@qq.com>
2024-11-18 08:58:49 +0800

    feat(uw-auth-center): 支持 Redis SSL 连接
    - 在 Redis 连接工厂中添加 SSL 配置支持
    - 更新多个模块的 Redis 连接配置方法
    - 提升 Redis 客户端版本以支持 SSL

--- src/main/java/uw/notify/center/conf/NotifyCenterAutoConfiguration.java
+++ src/main/java/uw/notify/center/conf/NotifyCenterAutoConfiguration.java
@@ -82,6 +82,7 @@ public class NotifyCenterAutoConfiguration {
                 config.setMaxWait( pool.getMaxWait() );
             }
             builder = LettucePoolingClientConfiguration.builder().poolConfig( config );
+
         }
 
         if (redisProperties.getTimeout() != null) {
@@ -94,15 +95,20 @@ public class NotifyCenterAutoConfiguration {
             }
         }
         builder.clientResources( clientResources );
-        LettuceClientConfiguration config = builder.build();
-
+        if (redisProperties.getSsl()!=null) {
+            builder.useSsl();
+//            LettuceClientConfiguration.LettuceSslClientConfigurationBuilder sslBuilder = builder.useSsl();
+//            if (redisProperties.getSsl().getBundle()!=null) {
+//            }
+        }
+        LettuceClientConfiguration clientConfig = builder.build();
         RedisStandaloneConfiguration standaloneConfig = new RedisStandaloneConfiguration();
         standaloneConfig.setHostName( redisProperties.getHost() );
         standaloneConfig.setPort( redisProperties.getPort() );
-        standaloneConfig.setPassword( RedisPassword.of( redisProperties.getPassword() ) );
         standaloneConfig.setDatabase( redisProperties.getDatabase() );
-
-        LettuceConnectionFactory factory = new LettuceConnectionFactory( standaloneConfig, config );
+        standaloneConfig.setPassword( RedisPassword.of( redisProperties.getPassword() ) );
+        standaloneConfig.setUsername( redisProperties.getUsername() );
+        LettuceConnectionFactory factory = new LettuceConnectionFactory( standaloneConfig, clientConfig );
         factory.afterPropertiesSet();
         return factory;
     }
