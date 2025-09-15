a2815fa88e872d70e7c98c177712a97a75a9c04f
zhangjin <23231269@qq.com>
2025-09-15 20:56:13 +0800

    build(uw-base): 更新基础版本号并修改配置密码
    -将 uw-base 版本号从 2025.0901.0000升级到 2025.0901.00

--- src/main/java/uw/notify/center/service/WebNotifyService.java
+++ src/main/java/uw/notify/center/service/WebNotifyService.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import uw.common.dto.ResponseData;
 import uw.common.util.JsonUtils;
@@ -19,15 +19,27 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * 通知服务。
  */
-@Component
+@Service
 public class WebNotifyService {
 
-    private static final Logger log = LoggerFactory.getLogger( WebNotifyService.class );
+    /**
+     * 日志。
+     */
+    private static final Logger log = LoggerFactory.getLogger(WebNotifyService.class);
 
+    /**
+     * SSE连接池。
+     */
     private static final ConcurrentHashMap<Long, SseEmitter> sseEmitterMap = new ConcurrentHashMap<>();
 
+    /**
+     * Redis配置。
+     */
     private static UwNotifyCenterProperties uwNotifyCenterProperties;
 
+    /**
+     * Redis模板。
+     */
     private static RedisTemplate<Long, Long> notifyRedisTemplate;
 
     public WebNotifyService(UwNotifyCenterProperties uwNotifyCenterProperties, RedisTemplate<Long, Long> notifyRedisTemplate) {
@@ -39,22 +51,22 @@ public class WebNotifyService {
      * 给指定用户发信息
      */
     public static ResponseData pushMsg(WebNotifyMsg webNotifyMsg, boolean autoRelay) {
-        SseEmitter se = sseEmitterMap.get( webNotifyMsg.getUserId() );
+        SseEmitter se = sseEmitterMap.get(webNotifyMsg.getUserId());
         if (se == null && autoRelay) {
             //如果本地不在线，并且开启了自动转发，则在redis中广播发送。
-            notifyRedisTemplate.convertAndSend( Constants.REDIS_NOTIFY_CHANNEL, JsonUtils.toString( webNotifyMsg ) );
+            notifyRedisTemplate.convertAndSend(Constants.REDIS_NOTIFY_CHANNEL, JsonUtils.toString(webNotifyMsg));
             return ResponseData.WARN;
         }
         if (se == null) {
-            return ResponseData.errorMsg( "指定用户[" + webNotifyMsg.getUserId() + "]不在线！" );
+            return ResponseData.errorMsg("指定用户[" + webNotifyMsg.getUserId() + "]不在线！");
         }
         try {
-            se.send( webNotifyMsg.getNotifyBody(), MediaType.APPLICATION_JSON );
+            se.send(webNotifyMsg.getNotifyBody(), MediaType.APPLICATION_JSON);
             se.complete();
             return ResponseData.SUCCESS;
         } catch (Exception e) {
-            log.error( "指定用户[{}]发送信息出错：{}", webNotifyMsg.getUserId(), e.getMessage(), e );
-            return ResponseData.errorMsg( e.getMessage() );
+            log.error("指定用户[{}]发送信息出错：{}", webNotifyMsg.getUserId(), e.getMessage(), e);
+            return ResponseData.errorMsg(e.getMessage());
         }
     }
 
@@ -64,7 +76,7 @@ public class WebNotifyService {
      * @return
      */
     public static List<Long> getOnlineIds() {
-        return new ArrayList<>( sseEmitterMap.keySet() );
+        return new ArrayList<>(sseEmitterMap.keySet());
     }
 
     /**
@@ -84,15 +96,15 @@ public class WebNotifyService {
      */
     public static SseEmitter openStream(long userId) {
         // 设置超时日期，0表示不过期
-        SseEmitter sseEmitter = new SseEmitter( uwNotifyCenterProperties.getSseTimeout() );
+        SseEmitter sseEmitter = new SseEmitter(uwNotifyCenterProperties.getSseTimeout());
         // 注册回调
-        sseEmitter.onCompletion( () -> removeUser( userId ) );
-        sseEmitter.onError( throwable -> {
-            removeUser( userId );
-            log.error( "SSE错误:{}", throwable.getMessage(), throwable );
-        } );
-        sseEmitter.onTimeout( () -> removeUser( userId ) );
-        sseEmitterMap.put( userId, sseEmitter );
+        sseEmitter.onCompletion(() -> removeUser(userId));
+        sseEmitter.onError(throwable -> {
+            removeUser(userId);
+            log.error("SSE错误:{}", throwable.getMessage(), throwable);
+        });
+        sseEmitter.onTimeout(() -> removeUser(userId));
+        sseEmitterMap.put(userId, sseEmitter);
         return sseEmitter;
     }
 
@@ -100,9 +112,9 @@ public class WebNotifyService {
      * 移出用户
      */
     private static void removeUser(long userId) {
-        sseEmitterMap.remove( userId );
+        sseEmitterMap.remove(userId);
         if (log.isTraceEnabled()) {
-            log.trace( "remove user id:{}, remain online count:{}. ", userId, sseEmitterMap.size() );
+            log.trace("remove user id:{}, remain online count:{}. ", userId, sseEmitterMap.size());
         }
     }
 
