e4370db8d13abfb36714c45d6e1ecc0c25ff5e9c
zhangjin <23231269@qq.com>
2025-09-15 17:58:56 +0800

    refactor(uw): 重构应用启动方式并更新版本
    - 使用 AppBootStrap 统一应用启动逻辑
    - 更新 uw-base版本至 2025.0901.0000
    - 移除自定义 BeanNameGenerator
    - 更新应用版本:
      - uw-auth-center: 6.1.11 -> 6.1.12
      - uw-code-center: 1.1.5 -> 1.1.6
      - uw-gateway: 1.1.7 -> 1.1.8
      - uw-gateway-center:1.1.5 -> 1.1.6
      - uw-lms-center: 2.1.5 -> 2.1.6
      - uw-mydb-center: 3.1.5 -> 3.1.6
      - uw-ops-center: 1.1.8 -> 1.1.9
      - uw-task-center: 5.1.5 -> 5.1.6  - uw-tinyurl-center: 更新 nacos 配置密码

--- src/main/java/uw/notify/center/UwNotifyCenterApplication.java
+++ src/main/java/uw/notify/center/UwNotifyCenterApplication.java
@@ -1,13 +1,15 @@
 package uw.notify.center;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import uw.common.app.AppBootStrap;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UwNotifyCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run( UwNotifyCenterApplication.class, args);
+        AppBootStrap.run(UwNotifyCenterApplication.class, args);
     }
 
 }
